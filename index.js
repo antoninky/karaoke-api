a54f8fe185385778f9347d45b7eef230d075e280
Demo <matthcollin6@gmail.com>
2024-06-08 19:46:58 +0200

    👌 IMPROVE: Chat implementation

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const socketIo = require("socket.io");
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-    });
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log(msg);
+    io.emit("messageSent", msg);
+  });
 });
 
 server.listen(8000, () => {
-    console.log('listening on *:8000');
+  console.log("listening on *:8000");
 });
-
