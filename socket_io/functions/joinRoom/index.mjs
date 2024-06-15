08ec1ae541dddd5b423b5404a4181377c7c2676b
MatthieuCollin <matthcollin6@gmail.com>
2024-06-15 03:37:13 +0200

    👌 IMPROVE: chckRoom on join

--- socket_io/functions/joinRoom/index.mjs
+++ socket_io/functions/joinRoom/index.mjs
@@ -1,5 +1,9 @@
+import { checkRooms } from "../../../app/functions/check_rooms/index.mjs";
+
 export function joinRoom(socket, io) {
   socket.on("join_room", (roomCode) => {
-    socket.join(roomCode);
+    if (checkRooms(roomCode)) {
+      socket.join(roomCode);
+    }
   });
 }
--- socket_io/functions/messages/index.mjs
+++ socket_io/functions/messages/index.mjs
@@ -1,6 +1,6 @@
 export function getMessages(socket, io) {
-  socket.on("chat message", (msg, room) => {
-    console.log({ message: msg, room: room });
-    io.to(room).emit("messageSent", msg);
+  socket.on("chat message", (username, msg, room) => {
+    console.log({ username: username, message: msg, room: room });
+    io.to(room).emit("messageSent", { username: username, message: msg });
   });
 }
