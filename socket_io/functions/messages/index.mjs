c88d2fbd21ef63073e262f4619f13c86e7333c34
MatthieuCollin <matthcollin6@gmail.com>
2024-06-15 02:26:48 +0200

    📦 NEW: room chat separation

--- socket_io/functions/messages/index.mjs
+++ socket_io/functions/messages/index.mjs
@@ -1,6 +1,6 @@
 export function getMessages(socket, io) {
-  socket.on("chat message", (msg) => {
-    console.log(msg);
-    io.emit("messageSent", msg);
+  socket.on("chat message", (msg, room) => {
+    console.log({ message: msg, room: room });
+    io.to(room).emit("messageSent", msg);
   });
 }
--- socket_io/index.mjs
+++ socket_io/index.mjs
@@ -1,7 +1,9 @@
+import { joinRoom } from "./functions/joinRoom/index.mjs";
 import { getMessages } from "./functions/messages/index.mjs";
 
 export function initIo(io) {
   io.on("connection", (socket) => {
+    joinRoom(socket, io);
     getMessages(socket, io);
   });
 }
